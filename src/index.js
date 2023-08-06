babe29697e321cf7f7a2e74e8ee8bfa2e41fc15f
Prabhleen Kaur Saini <prabhleenkaursaini@Prabhleens-MBP.lan>
2023-08-05 23:55:24 -0500

    using react v17

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <Auth0Provider
     domain="dev-iqjnmzs6nb51u1l8.us.auth0.com"
     clientId="FWvAvzl9NlOrQDQ3vdd8NrXkfiSZmKRk"
     redirectUri={window.location.origin}
   >
-    <App />
-  </Auth0Provider>
+    <App />,
+  </Auth0Provider>,
+  document.getElementById("root")
 );
